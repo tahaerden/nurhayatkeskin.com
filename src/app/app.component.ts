1a84a01371fdfdbd5f7673ebb51563a68b09b692
tahaerden <tahaerden@windowslive.com>
2021-01-01 16:29:32 +0300

    Created initial structure.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'nurhayat-keskin';
+export class AppComponent implements OnInit {
+  posts: any;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.posts = [];
+    for (let i = 1; i < 10; i++) {
+      this.http.get(`assets/blog/blog${i}.txt`, { responseType: 'text' }).subscribe(
+        (data) => {
+          const headerPos = data.indexOf('\r\n');
+          const header = data.slice(0, headerPos);
+          const content = data.slice(headerPos);
+          // const header = data.split('\r\n')[0];
+          // const content = data.split('\r\n').slice(1).join('\r\n');
+          this.posts.push({
+            header,
+            content
+          });
+        },
+        (error) => of(false)
+      );
+    }
+  }
 }
