af50fa2cf099136f72a847399e57fb3c11777799
tahaerden <tahaerden@windowslive.com>
2021-01-03 14:19:47 +0300

    Fixes.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,11 +17,9 @@ export class AppComponent implements OnInit {
     for (let i = 1; i < 10; i++) {
       this.http.get(`assets/blog/${i}.txt`, { responseType: 'text' }).subscribe(
         (data) => {
-          const headerPos = data.indexOf('\r\n');
+          const headerPos = data.indexOf('\n');
           const header = data.slice(0, headerPos);
           const content = data.slice(headerPos);
-          // const header = data.split('\r\n')[0];
-          // const content = data.split('\r\n').slice(1).join('\r\n');
           this.posts.push({
             header,
             content
